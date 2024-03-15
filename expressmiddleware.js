f6e8bd8c3b6136ae3c14b65c90cc89f5009c13ff
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-03-15 17:17:34 +0530

    Create expressmiddleware.js

---
+++ expressmiddleware.js
@@ -0,0 +1,63 @@
+/*
+user authentication choices
+
+session
+json web tokens
+oauth (In-house and saas)(auth0)
+other/ad-hoc
+
+*/
+
+/*
+passport js
+express middleware to handle route permission
+401 error
+uses cookies,sessions, some logic
+see passportjs website
+*/
+
+/*
+http headers
+general,
+request,
+response,
+*/
+
+/*
+cookies are a way to store data in the browser
+example some sites send some cookies after login and store them as cookies
+now when we make an api request now we can attach cookies in the frontend using these cookies
+
+*/
+
+/*
+express middleware
+
+function say(err,req,res,next){
+  if(err){
+    console.log(err);
+  }
+}
+
+we can send data in the next({name: "saikrishna"})
+
+app.use(errorHandler);
+this should be at the last line of the code
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
