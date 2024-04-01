acce946d31aef39f60526fc835276d4e4899f195
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-04-01 21:13:19 +0530

    Create day1.js

---
+++ css/day1.js
@@ -0,0 +1,66 @@
+/*
+1.started by teaching how to import styles.css file
+2. element selector 
+h1{
+  color: red
+}
+.title {
+  color: blue
+}
+
+.center {
+  text-align: center
+}
+we can use same class as many times as we want
+
+we can combine many classses as we want
+*/
+
+/*
+BOX MODEL
+
+div: we use it to structure our code
+lorem10
+
+border : 
+margin : space outside the box
+padding : space inside the box
+border-radius: 30px
+
+unsplash.com for images
+
+for images 
+use .box-img{
+  width: 100%;
+  border-radius: 14px;
+}
+
+google news
+codekaro.com
+amazon.in login page
+linked.com
+
+auto works with left and right only and not with top and bottom
+font-family: system-ui
+
+button css
+
+login page
+
+box-sizing: border-box
+width: 100%
+padding: 16px;
+border-radius: 12px;
+
+one.google.com
+(card)
+
+border-top-left-radius: 14px;
+border-top-right-radius: 14px;
+
+
+*/
+
+
+
+
