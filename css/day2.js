bb183601a9290b9631a9495a8f314dfeb9a6b56d
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-04-02 21:28:19 +0530

    Create day2.js

---
+++ css/day2.js
@@ -0,0 +1,43 @@
+/*
+CSS POSITIONS 
+position: placement of an object in a particular area
+
+4types of positions: 
+fixed
+absolute
+sticky
+relative
+
+"https://source.unsplash.com/1600*1000/?burger"
+
+fixed: create a button and fix it in the right bottom 
+display: fixed;
+bottom: 0;
+right: 0;
+
+top,right,bottom and left work only with positions.
+
+position: sticky
+
+
+sticky vs fixed
+fixed is always fixed
+sticky becomes sticky only at the mentioned co ordinates
+
+
+
+position absolute
+top:0;
+right:0;
+
+relative ensures that child elements (absolute) move inside the parent element
+
+we can put as many absolute inside 1 relative position
+
+*/
+
+
+
+
+
+
