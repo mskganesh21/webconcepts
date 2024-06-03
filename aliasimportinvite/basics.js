92e4c29df6d34953c8a353ce40908311e8a0c2a7
mskganesh21 <157506559+mskganesh21@users.noreply.github.com>
2024-06-03 17:04:04 +0530

    Create basics.js

---
+++ aliasimportinvite/basics.js
@@ -0,0 +1,56 @@
+/*
+add path alias in resolve in vite.config.ts file
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+    }
+  }
+});
+ 
+
+also add alias in path resolve in tsconfig.json in compiler options in path 
+
+{
+  "compilerOptions": {
+    "paths": {
+      "@components/*": [
+        "./src/components/*"
+      ], 
+      "@assets/*": [
+        "./src/assets/*"
+      ]
+    },
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "module": "ESNext",
+    "skipLibCheck": true,
+
+    /* Bundler mode */
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "resolveJsonModule": true,
+    "isolatedModules": true,
+    "noEmit": true,
+    "jsx": "react-jsx",
+
+    /* Linting */
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": ["src"],
+  "references": [{ "path": "./tsconfig.node.json" }]
+}
+
+
+*/
